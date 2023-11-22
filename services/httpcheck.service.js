02f8cd231558a56bd5659ace1c53059a1833ecad
Karan Wadhwa <wadhwa.karan96@gmail.com>
2023-11-22 16:35:16 -0500

    fix: update httpcheck model
    added http_check_id and removed is_paused, uptime_sla, use_ssl,
    check_interval_in_seconds

--- services/httpcheck.service.js
+++ services/httpcheck.service.js
@@ -2,26 +2,28 @@ import HttpCheckModel from '../models/httpcheck.model.js'
 
 const create = async (params) => {
   const {
+    http_check_id,
     name,
     uri,
-    is_paused,
     num_retries,
-    uptime_sla,
-    response_time_sla,
-    use_ssl,
+    // is_paused,
+    // uptime_sla,
+    // response_time_sla,
+    // use_ssl,
     response_status_code,
-    check_interval_in_seconds,
+    // check_interval_in_seconds,
   } = params
   const save = await HttpCheckModel.create({
+    http_check_id,
     name,
     uri,
-    is_paused,
     num_retries,
-    uptime_sla,
-    response_time_sla,
-    use_ssl,
+    // is_paused,
+    // uptime_sla,
+    // response_time_sla,
+    // use_ssl,
     response_status_code,
-    check_interval_in_seconds,
+    // check_interval_in_seconds,
   })
   return save
 }
